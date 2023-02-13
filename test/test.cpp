2b87ff4eea0d2eb54d74f669bb487ea9c73a681c
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2023-02-13 15:43:48 +0100

    :memo: small update

---
+++ test/test.cpp
@@ -0,0 +1,10 @@
+#include <catch2/catch_template_test_macros.hpp>
+
+#include "../src/interface.h"
+
+
+TEST_CASE("Factorials are computed", "[QuickSimInterface]")
+{
+    QuickSimInterface testclass{"test","test","test",5};
+    testclass.runSimulation();
+}
