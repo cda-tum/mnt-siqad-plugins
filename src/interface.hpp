05f5c4bca51952e0cda914c46e34f1ee43db64ac
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2023-02-27 08:01:01 +0100

    :art: reformat code

--- src/interface.hpp
+++ src/interface.hpp
@@ -134,12 +134,12 @@ class quicksim_interface
         sim_par = fiction::quicksim_params{params, iteration_steps, alpha};
 
         // Check if number_threads is negative
-        if (const auto number_threads = static_cast<int64_t>(std::stod(sqconn->getParameter("num_threads"))); number_threads >= 0)
+        if (const auto number_threads = static_cast<int64_t>(std::stod(sqconn->getParameter("num_threads")));
+            number_threads >= 0)
         {
             // Update sim_par with number_threads
             sim_par.number_threads = static_cast<uint64_t>(number_threads);
         }
-
 
         log.echo() << "Retrieval from SiQADConn complete." << std::endl;
     }
