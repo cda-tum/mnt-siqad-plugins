47ea9a6e74cd9d50ed14e3680b5f0bef76089fa0
Drewniok <jan.drewniok.reseaqrch@gmail.com>
2023-03-15 09:20:42 +0100

    :bug: small changes in format

--- test/interface.cpp
+++ test/interface.cpp
@@ -27,22 +27,16 @@ TEST_CASE("Test if reading, simulating, and creating a result-file works", "[int
 
     CHECK(qs_interface.run_simulation() == 0);
     CHECK(!qs_interface.get_simulation_results().valid_lyts.empty());
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].num_cells() == 7);
+    const auto& charge_lyt_first = qs_interface.get_simulation_results().valid_lyts.front();
+    CHECK(charge_lyt_first.num_cells() == 7);
 
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({60, 23, 0}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({58, 20, 1}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({63, 21, 1}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({57, 24, 1}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({56, 23, 0}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({53, 21, 1}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({50, 24, 0}) ==
-          fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({60, 23, 0}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({58, 20, 1}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({63, 21, 1}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({57, 24, 1}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({56, 23, 0}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({53, 21, 1}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
+    CHECK(charge_lyt_first.get_cell_type({50, 24, 0}) == fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
 
     qs_interface.write_sim_results();
 
