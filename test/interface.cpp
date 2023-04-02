c367a36e28f00da481e859688df6f28c5cb6de9b
Jan Drewniok <97012901+Drewniok@users.noreply.github.com>
2023-04-02 18:02:07 +0200

    :bug: factor of 10E-9 was missing for lambda_tf. (#44)
    Co-authored-by: Drewniok <jan.drewniok.reseaqrch@gmail.com>

--- test/interface.cpp
+++ test/interface.cpp
@@ -5,6 +5,7 @@
 #include "interface.hpp"
 
 #include <catch2/catch_template_test_macros.hpp>
+#include <catch2/matchers/catch_matchers_floating_point.hpp>
 
 #include <fiction/types.hpp>
 
@@ -18,7 +19,8 @@ TEST_CASE("Test if reading, simulating, and creating a result-file works", "[int
     auto qs_interface = quicksim_interface{fmt::format("{}/sim_problem_0.xml", TEST_PATH),
                                            fmt::format("{}/sim_result_0.xml", TEST_PATH), false};
 
-    CHECK(qs_interface.get_quicksim_params().phys_params.lambda_tf == 5);
+    CHECK_THAT(qs_interface.get_quicksim_params().phys_params.lambda_tf,
+               Catch::Matchers::WithinAbs(5 * 10E-9, fiction::physical_constants::POP_STABILITY_ERR));
     CHECK(qs_interface.get_quicksim_params().phys_params.epsilon_r == 5.6);
     CHECK(qs_interface.get_quicksim_params().phys_params.mu == -.25);
     CHECK(qs_interface.get_quicksim_params().interation_steps == 70);
