06b20fa93c3a80bc0b333d38ff66b352aec80b3c
Jan Drewniok <97012901+Drewniok@users.noreply.github.com>
2023-08-10 10:41:07 +0200

    ⬆️ Update fiction as submodule (#90)
    * :arrow_up: update ``fiction``.

--- test/interface.cpp
+++ test/interface.cpp
@@ -22,27 +22,27 @@ TEST_CASE("Test if reading, simulating, and creating a result-file works", "[int
     CHECK_THAT(qs_interface.get_quicksim_params().phys_params.lambda_tf,
                Catch::Matchers::WithinAbs(5, fiction::physical_constants::POP_STABILITY_ERR));
     CHECK(qs_interface.get_quicksim_params().phys_params.epsilon_r == 5.6);
-    CHECK(qs_interface.get_quicksim_params().phys_params.mu == -.25);
+    CHECK(qs_interface.get_quicksim_params().phys_params.mu_minus == -.25);
     CHECK(qs_interface.get_quicksim_params().interation_steps == 70);
     CHECK(qs_interface.get_quicksim_params().alpha == 0.8);
     CHECK(qs_interface.get_quicksim_params().number_threads == std::thread::hardware_concurrency());
 
     REQUIRE(qs_interface.run_simulation() == 0);
-    REQUIRE(!qs_interface.get_simulation_results().valid_lyts.empty());
+    REQUIRE(!qs_interface.get_simulation_results().charge_distributions.empty());
 
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({60, 23, 0}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({60, 23, 0}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({58, 20, 1}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({58, 20, 1}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({63, 21, 1}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({63, 21, 1}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({57, 24, 1}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({57, 24, 1}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({56, 23, 0}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({56, 23, 0}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({53, 21, 1}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({53, 21, 1}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
-    CHECK(qs_interface.get_simulation_results().valid_lyts[0].get_cell_type({50, 24, 0}) ==
+    CHECK(qs_interface.get_simulation_results().charge_distributions[0].get_cell_type({50, 24, 0}) ==
           fiction::sidb_cell_clk_lyt_siqad::cell_type::NORMAL);
 
     qs_interface.write_sim_results();
